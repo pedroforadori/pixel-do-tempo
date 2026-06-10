0f3abc0cd4bb02fece57f356973367ff84966b37
Pedro Foradori <analistamobile.logtech@trouw.com.br>
2026-06-10 17:41:46 -0300

    chore: configure tooling (biome, next, tailwind)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+        pathname: "/storage/v1/object/**",
+      },
+      {
+        protocol: "https",
+        hostname: "replicate.delivery",
+      },
+      {
+        protocol: "https",
+        hostname: "pbxt.replicate.delivery",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
